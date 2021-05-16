167c8478be64f70758bf6da2db92460f06aa7fc3
github <nhutranle59@gmail.com>
2021-05-16 07:29:10 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,14 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		int number;
+		String nameSchool;
+		
+	}
+	public void show() {}
+}
